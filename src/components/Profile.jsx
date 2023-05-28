fb506abf8bb113e881e0cf83609d221e16d5704c
raminta kupcinskaite <raminta.kupcinskaite@gmail.com>
2023-05-28 23:00:54 +0300

    updated profile and directory

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,42 +1,12 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { Logo, ProfileTitle, ProfilePar } from "../styles/StyledProfile";
 
-export const Profile = ({ currentUser }) => {
-  const [employee, setEmployee] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchEmployeeData = async () => {
-      try {
-        const token = localStorage.getItem("token");
-        const response = await axios.get(
-          `http://localhost:5000/profile/${currentUser.id}`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        setEmployee(response.data.employee);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    if (currentUser) {
-      // console.log(currentUser)
-      fetchEmployeeData();
-    }
-  }, [currentUser]);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+export const Profile = () => {
 
   return (
     <>
-      <h1>Welcome, {employee.firstName} {employee.lastName}!</h1>
+        <Logo src="./glasslogo.png"></Logo>
+        <ProfileTitle>Welcome!</ProfileTitle>
+        <ProfilePar>Customer Service Representative</ProfilePar>
     </>
   );
 };
